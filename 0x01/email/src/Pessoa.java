bd751cced4506a18b090efb0e0312deb8a14191e
eniofrancisco <eniofrancisco@gmail.com>
2025-10-30 21:54:45 +0000

    junit

---
+++ 0x01/email/src/Pessoa.java
@@ -0,0 +1,11 @@
+package email;
+
+public class Pessoa {
+
+    public static boolean emailValid(String email) {
+        if (email == null) return false;
+        if (!email.contains("@")) return false;
+        if (email.length() > 50) return false;
+        return true;
+    }
+}
